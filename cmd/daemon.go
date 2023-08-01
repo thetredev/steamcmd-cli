5c416578c1e27a496ce7e38c18016cd313f9d310
Timo Reichl <thetredev@gmail.com>
2023-08-01 20:59:42 +0200

    daemon: Add CLI input options for CA certificate, daemon certificate & key
    Signed-off-by: Timo Reichl <thetredev@gmail.com>

--- cmd/daemon.go
+++ cmd/daemon.go
@@ -7,7 +7,7 @@ import (
 )
 
 var daemonCmd = &cobra.Command{
-	Use:     "daemon",
+	Use:     "daemon <CA certificate input file path> <Daemon certificate input key path>",
 	Version: shared.Version,
 	Short:   "Run the daemon socket",
 	Run:     daemonCallback,
@@ -18,5 +18,10 @@ func init() {
 }
 
 func daemonCallback(cmd *cobra.Command, args []string) {
-	daemon.StartSocket()
+	if len(args) < 3 {
+		cmd.Help()
+		return
+	}
+
+	daemon.StartSocket(args[0], args[1], args[2])
 }
